b43a950cfa15728c9fb64fab17580a61c1fe605c
DIG <kiru42@gmail.com>
2016-08-01 01:33:41 +0200

    Deploy in prod :)

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :posts
   devise_for :users
-  root to: 'pages#index'
+  root to: 'posts#index'
   resources :contacts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
