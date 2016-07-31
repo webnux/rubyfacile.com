17a5aa2ff06a4590f52304a7ff71a4066cc5371d
DIG <kiru42@gmail.com>
2016-07-31 14:23:32 +0200

    capitrano updated

--- config/deploy.rb
+++ config/deploy.rb
@@ -48,14 +48,13 @@ set :rbenv_path, '/home/deploy/.rbenv/'
 
 namespace :deploy do
 
-  desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      execute :touch, release_path.join('tmp/restart.txt')
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      # Here we can do anything such as:
+      # within release_path do
+      #   execute :rake, 'cache:clear'
+      # end
     end
   end
-
-  after :publishing, 'deploy:restart'
-  after :finishing, 'deploy:cleanup'
 
 end
